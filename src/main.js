3c86d5e7e18cc72568c7e65daa560f0df94279e8
paulalopezruiz <paula.lopez51397@ikasle.egibide.org>
2026-05-08 19:59:33 +0200

    Proyecto Vue inicial

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import './assets/main.css'
+
+import '@/assets/bootstrap.scss';
+import * as bootstrap from 'bootstrap';
+
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
